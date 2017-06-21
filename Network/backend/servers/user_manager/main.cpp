e46771cb12071a7e6ccefb5ed7cb982e8577b89a
evilsmile <freedominmind@163.com>
2017-06-21 11:13:50 +0800

    return fail if name not exists. and make user_manager load config_file from argv

--- Network/backend/servers/user_manager/main.cpp
+++ Network/backend/servers/user_manager/main.cpp
@@ -40,46 +40,62 @@ int send_reply(int fd, const std::string& reply)
 
 int handle_request(ClientConn client_conn)
 {
-    std::string request = client_conn.getData();
-    key_value_t params;
+    int error = 0;
+    do {
+        std::string request = client_conn.getData();
+        key_value_t params;
 
-    splitStringToMap(request, '&', '=', params);
+        splitStringToMap(request, '&', '=', params);
 
-    key_value_t::iterator iter = params.find("name");
-    if (iter == params.end()) 
-    {
-        log_error("param error: miss 'name'");
-        return -1;
-    }
-    iter = params.find("age");
-    if (iter == params.end()) 
-    {
-        log_error("param error: miss 'age'");
-        return -1;
-    }
+        key_value_t::iterator iter = params.find("name");
+        if (iter == params.end()) 
+        {
+            log_error("param error: miss 'name'");
+            error = -1;
+            break;
+        }
+        iter = params.find("age");
+        if (iter == params.end()) 
+        {
+            log_error("param error: miss 'age'");
+            error = -1;
+            break;
+        }
 
-    std::string name = params["name"];
-    std::string age = params["age"];
+        std::string name = params["name"];
+        std::string age = params["age"];
 
-    MysqlRowSetPtr res = mysqlApi.query("SELECT age FROM es.lovers WHERE name='%s'", name.c_str());
+        MysqlRowSetPtr res = mysqlApi.query("SELECT age FROM es.lovers WHERE name='%s'", name.c_str());
+        if (res.get() == NULL || !res->next()) {
+            log_error("User '%s' NOT-FOUND", name.c_str());
+            error = -1;
+            break;
+        }
 
-    if (mysqlApi.update("UPDATE es.lovers SET age=%s WHERE name='%s'",  age.c_str(), name.c_str()) < 0) {
-        log_error("update failed.");
-        return -2;
-    }
+        if (mysqlApi.update("UPDATE es.lovers SET age=%s WHERE name='%s'",  age.c_str(), name.c_str()) < 0) {
+            log_error("update failed.");
+            error = -2;
+            break;
+        }
 
-    send_reply(client_conn.getFD(), "SUCC");
+    } while(0);
 
-    return 0;
+    send_reply(client_conn.getFD(), error ? "FAILED" : "SUCC");
+
+    return error;
 }
 
-int main()
+int main(int argc, char* argv[])
 {
     init_log("user_manager");
 
     int error = 0;
 
-    error = config_parser.loadFile("../../config/" + Const::CONFIG_FILENAME);
+    std::string standard_config_file = Const::CONFIG_FILENAME;
+    if (argc > 1) {
+        standard_config_file = argv[1];
+    }
+    error = config_parser.loadFile(standard_config_file);
     if (error) {
         return -1;
     }
