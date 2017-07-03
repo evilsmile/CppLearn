b8f40e1b205c8ebec3c3291ce13c9f8ff364161c
evilsmile <freedominmind@163.com>
2017-07-03 21:36:10 +0800

    add redis test by nginx-lua

--- Network/backend/config/nginx/test.sh
+++ Network/backend/config/nginx/test.sh
@@ -2,11 +2,13 @@
 
 
 # Nginx - Lua 测试
-curl 'localhost/lua_calc?num1=23&num2=234'
-curl 'localhost/lua_hello?who=world'
-curl 'localhost/lua_file?a=23&b=834'
+#curl 'localhost:81/lua_calc?num1=23&num2=234'
+#curl 'localhost:81/lua_hello?who=world'
+#curl 'localhost:81/lua_file?a=23&b=834'
 
-curl 'localhost/lua_auth?user=ntes'
-#curl 'localhost/lua_auth?user=soso'
+#curl 'localhost:81/lua_auth?user=ntes'
+#curl 'localhost:81/lua_auth?user=soso'
 
-curl 'localhost/lua_call_subreq'
+#curl 'localhost:81/lua_call_subreq'
+
+curl 'localhost:81/lua_redis?key=mood'
