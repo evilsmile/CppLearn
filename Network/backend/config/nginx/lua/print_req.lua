814b230a8e4a8b2ae9802af66540c2b004325aa9
evilsmile <freedominmind@163.com>
2017-07-06 10:37:39 +0800

    add lua shared memory test, and some more API test

--- Network/backend/config/nginx/lua/print_req.lua
+++ Network/backend/config/nginx/lua/print_req.lua
@@ -8,6 +8,17 @@ function print_t(t)
 	end
 end
 
+---------- 写响应头 --------------
+ngx.header.a = "Reply_A";
+ngx.header.b = {"Reply_B1", "Reply_B2"};
+
+-- 会以 [322] 结束, 不往下执行了。  
+-- ngx.exit(322);
+
+-- 设置响应码
+ngx.status=303
+
+---------- 写响应体 ------------
 local var = ngx.var;
 
 ngx.say("ngx var a: ", var.a, "<br/>");
@@ -54,7 +65,23 @@ end
 ngx.say("post args end", "<br/>");
 
 
-ngx.say("http version: ", ngx.req.http_version(), "<br/>");
-ngx.say("http get_method ", ngx.req.get_method(), "<br/>");
-ngx.say("http raw_header ", ngx.req.raw_header(), "<br/>");
-ngx.say("http body data ", ngx.req.get_body_data(), "<br/>");
+------------- other --------------
+---- ngx.print 和 ngx.say 都是输出响应体，但是ngx.print不打印最后的换行符
+ngx.print("http version: ", ngx.req.http_version(), "<br/>");
+ngx.print("http get_method ", ngx.req.get_method(), "<br/>");
+ngx.print("http raw_header ", ngx.req.raw_header(), "<br/>");
+ngx.print("http body data ", ngx.req.get_body_data(), "<br/>");
+
+local request_uri = ngx.var.request_uri;
+ngx.say("ngx request uri: ", request_uri, "<br/>");
+ngx.say("unescape uri: ", ngx.unescape_uri(request_uri), "<br/>");
+ngx.say("md5(123): ", ngx.md5("123"), "<br/>");
+ngx.say("http time: ", ngx.http_time(ngx.time()), "<br/>");
+ngx.say("encode args ", ngx.encode_args({a1="arg1", a2="arg2"}), "<br/>");
+
+local decoded_uri=ngx.decode_args("arg1=day1&arg2= monday");
+print_t(decoded_uri);
+
+local base64_encode = ngx.encode_base64("321");
+ngx.say("base64 encode: ", base64_encode);
+ngx.say("base64 decode: ", ngx.decode_base64(base64_encode));
--- Network/backend/config/nginx/test.sh
+++ Network/backend/config/nginx/test.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+opt=""
+if [ $# -gt 0 ]; then
+    opt=$1
+fi
 
 # Nginx - Lua 测试
 #curl 'localhost:81/lua_calc?num1=23&num2=234'
@@ -16,4 +20,7 @@
 #curl 'localhost:81/lua_ver'
 
 #curl -A "Spalding sent HELLLO" 'localhost:81/lua_content_file/1/8' -d "post_param=333&post_param2=777"
-curl -A "Spalding sent HELLLO" 'localhost:81/lua_content_file/1/8?arg_param=123&arg_param2=334' -d "post_param=333&post_param2=777"
+#curl $opt -A "Spalding sent HELLLO" 'localhost:81/lua_content_file/1/8?arg_param=1 23&arg_param2=334' -d "post_param=333&post_param2=777"
+
+#curl $opt 'localhost:81/lua_redirect'
+curl $opt 'localhost:81/lua_shared_data'
