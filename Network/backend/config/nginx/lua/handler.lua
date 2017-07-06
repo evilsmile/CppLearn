bcd588497b32baad7dd09085a8bbbb521070174f
evilsmile <freedominmind@163.com>
2017-07-06 17:49:29 +0800

    adjust format

--- Network/backend/config/nginx/lua/handler.lua
+++ Network/backend/config/nginx/lua/handler.lua
@@ -5,7 +5,14 @@ if not user then
     ngx.exit(200);
 end
 
-local get_user_info_res, get_user_level_res = ngx.location.capture_multi({{'/get_user_info/' .. user}, {'/get_user_level/' .. user}}); 
+local get_user_info_res, get_user_level_res = ngx.location.capture_multi({
+        {
+            '/get_user_info/' .. user
+        },
+        {
+            '/get_user_level/' .. user
+        }
+   }); 
 
 if get_user_info_res.status == 200 then
     ngx.print(get_user_info_res.body);
